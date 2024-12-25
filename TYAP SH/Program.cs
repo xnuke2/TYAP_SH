4c5fd51ecb13fe049b86150e9a4d50ebad02c071
xnuke2 <xmnikit@gmail.com>
2024-12-25 05:44:55 +0300

    fix

--- TYAP SH/Program.cs
+++ TYAP SH/Program.cs
@@ -11,15 +11,8 @@ namespace TYAP_SH
     {
         static void Main(string[] args)
         {
-            //if (Regex.IsMatch("BEGINa=1END", "^BEGIN(READ\\((([a-z]+(,[a-z]+)+))\\)|WRITE\\((([a-z]+(,[a-z]+)+))\\)|([a-z]+=([0-9]|[a-z]|[\\+]|[\\-]|[\\*]|[\\(]|[\\)])+))(;(READ\\((([a-z]+(,[a-z]+)+))\\)|WRITE\\((([a-z]+(,[a-z]+)+))\\)|([a-z]+=([0-9]|[a-z]|[\\+]|[\\-]|[\\*]|[\\(]|[\\)])+)))*END$"))
-            //    Console.WriteLine("yes");
-            //else Console.WriteLine("no");
-            if (Regex.IsMatch("READ(b,c)", "^READ\\(([a-z]+(,[a-z]+)*)\\)$"))
-                Console.WriteLine("yes");
-            else Console.WriteLine("no");
-            //"VAR a,b,c:INTEGER; BEGIN a =1;READ(b,c);WRITE(a,b);FOR a=-2 TO b DO c=-a*b-1+2) END_FOR END"
             Translator translator = new Translator();
-            translator.Tranclste("VAR a,b,c:INTEGER; BEGIN a =1;READ(b,c);WRITE(a,b);FOR a=-2 TO b DO c=(-a*b-1+2) END_FOR END");
+            translator.Tranclste("VAR a,b,c:INTEGER; BEGIN ab =1;READ(b,c);WRITE(a,b);FOR a=-2 TO b DO c=(-a*b-1+2) END_FOR END");
         }
     }
 }
